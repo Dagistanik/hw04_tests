755f83a48ea06d378120861eff138c3de187a59f
Dagistanik <sotnichenko.danik@mail.ru>
2021-12-15 11:38:15 +0300

    first commit

---
+++ yatube/posts/tests/test_forms.py
@@ -0,0 +1,60 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from posts.models import Post, Group
+from django.urls import reverse
+
+
+User = get_user_model()
+
+
+class PostCreateFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create(username='admin')
+        cls.authorized_client = Client()
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            group=cls.group,
+            text='Тестовый текст',
+        )
+
+    def setUp(self):
+        self.authorized_client.force_login(self.user)
+
+    def test_create_post(self):
+        """Валидная форма создает запись в Post и происходит редирект"""
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Тестовый тескт_1',
+            'group': self.group.pk,
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        self.assertRedirects(
+            response, reverse('posts:profile', args=['admin'])
+        )
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+
+    def test_edit_post(self):
+        """Происходит изменение поста"""
+        post_id = self.post.pk
+        expected_text = 'Изменённый текст'
+        form_data = {
+            'text': expected_text,
+            'group': self.group.pk,
+        }
+        self.authorized_client.post(
+            reverse('posts:post_edit', args=(str(post_id))),
+            data=form_data,
+            follow=True
+        )
+        self.assertEqual(Post.objects.get(pk=post_id).text, expected_text)
